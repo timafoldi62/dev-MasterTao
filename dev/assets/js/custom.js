8a1a50b8337ad1f48c68d0cc8faa26b9c25e23c3
timafoldi62 <timafoldi62@gmail.com>
2023-10-30 18:51:29 +0200

    test commit

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,31 @@
-  
+const burger = document.querySelector(".hamburger");
+burger.addEventListener("click", function () {
+	burger.classList.toggle("is-active");
+});
+
+// slider
+$(".first-screen-slider").slick({
+	dots: true,
+	infinite: true,
+	appendArrows: ".first-screen-nav",
+	responsive: [
+		{
+			breakpoint: 991,
+			settings: {
+				arrows: false,
+			},
+		},
+	],
+});
+
+//upload-file
+
+document.getElementById("fileInput").onchange = function () {
+	document.getElementById("file-name").innerHTML = this.files[0].name;
+};
+
+const checkButton = document.querySelectorAll('[type="checkbox"]');
+
+checkButton.forEach((item, index) => {
+	index === 0 ? item.closest(".input-box").classList.add("pos-checkbox") : null;
+});
