0e9c9f9c0c9b12ab88c842b38e83f149616d8705
timafoldi62 <timafoldi62@gmail.com>
2023-11-11 21:57:10 +0200

    slider

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -20,6 +20,34 @@ $(".first-screen-slider").slick({
 
 //upload-file
 
+mobileOnlySlider("#advantages-slider", true, false, 991);
+
+function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
+	var slider = $($slidername);
+	var settings = {
+		mobileFirst: true,
+		dots: $dots,
+		arrows: $arrows,
+		responsive: [
+			{
+				breakpoint: $breakpoint,
+				settings: "unslick",
+			},
+		],
+	};
+
+	slider.slick(settings);
+
+	$(window).on("resize", function () {
+		if ($(window).width() > $breakpoint) {
+			return;
+		}
+		if (!slider.hasClass("slick-initialized")) {
+			return slider.slick(settings);
+		}
+	});
+} // Mobile Only Slider
+
 document.getElementById("fileInput").onchange = function () {
 	//short name
 	// document.getElementById('file-name').innerHTML = this.files[0].name;
